dca32e3733c6df81fc58dcd710e44f65b29be477
Muhammad Arif <arif1951995@gmail.com>
2019-11-25 13:15:59 -0800

    asasd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,20 @@ import Button from "@material-ui/core/Button";
 
 class Navbar extends Component {
   render() {
+    
     return (
       <AppBar className="AppBar">
         <ToolBar className="nav-container">
           <Button color="inherit" component={Link} to="/login">
             Login
           </Button>
-          <Button color="inherit" component={Link} to="/signup">
-            Signup
-          </Button>
           <Button color="inherit" component={Link} to="/">
             Home
           </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        
         </ToolBar>
       </AppBar>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 
  class home extends Component {
+    state = {
+        posts: null,
+    }
+
+    componentDidMount() {
+        axios.get('https://asia-east2-fakebook-39e52.cloudfunctions.net/api/posts')
+        .then(data => {
+            this.setState({posts: data.data})
+            console.log(data.data);
+        })
+        .catch(err => {
+            console.log(err);
+        }) 
+    }
+
     render() {
+    
         return (
             <Grid container spacing={3}>
                 <Grid item sm={8} xs={12}>
                     <p>Content...</p>
+                    <p></p>
                 </Grid>
                 <Grid item sm={4} xs={12}>
                     <p>Profile</p>
                 </Grid>
-
+            
             </Grid>
         )
     }
